6386f6cae695135bb30af0709ae450dc2abd409e
Pär Gregersson <par@binarypeak.se>
2012-01-13 14:13:07 +0100

    Fixed incorrect paths when cloning kd-ext.

--- init.el
+++ init.el
@@ -1,19 +1,23 @@
 ;; Required variables!
-(setq load-path (cons "~/konfig/" load-path))
-(setq el-get-user-package-directory "~/konfig/package-configs")
+(setq konfig-home "/Users/greget/konfig/")
+(setq load-path (cons konfig-home load-path))
+(setq el-get-user-package-directory "Users/greget/konfig/package-configs")
 ;; -- end of Required variables
 
 ;; Load el-get packages
 (load-library "setup-el-get.el")
 
 ;; Clone kd-repos if needed
-(if (not (file-exists-p "kd-repos"))
+(if  (file-exists-p (concat konfig-home "kd-repos"))
     (progn 
-      (call-process "hg" nil "messages" nil "clone" "https://bitbucket.org/dnils/kd" "kd-repos/kd")
-      (call-process "hg" nil "messages" nil "clone" "https://bitbucket.org/dnils/kdext" "kd-repos/kdext")
+      ;; The following will be done if the kd-repos are present
+      (load-library "kd-repos/kdext/kdext-autosave-settings.el")  ;; Avoid ugly tmp-files
       )
+  (progn 
+    ;; There are no kd-repos, clone them!
+    (dired-create-directory (concat konfig-home "kd-repos"))
+    (call-process "hg" nil "messages" nil "clone" "https://bitbucket.org/dnils/kd" (concat konfig-home "kd-repos/kd"))
+    (call-process "hg" nil "messages" nil "clone" "https://bitbucket.org/dnils/kdext" (concat konfig-home "kd-repos/kdext"))
+    )
   )
-
-(load-library "kd-repos/kdext/kdext-autosave-settings.el")  ;; Avoid ugly tmp-files
-
 
